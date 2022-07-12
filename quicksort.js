115d55d357476570802e639bebf650856a03caaa
iamseye <iamseye@gmail.com>
2022-07-12 13:46:34 -0400

    chore: added quick sort

---
+++ quicksort.js
@@ -0,0 +1,26 @@
+function quickSort(array) {
+    if(!array.length) {
+        return [];
+    }
+
+    const pivotIndex = Math.floor(array.length/2);
+    const pivot = array[pivotIndex];
+
+    let left = [];
+    let right = [];
+
+    for (let i = 0; i<array.length ; i++) {
+        if(i === pivotIndex) {
+            continue;
+        }
+        const item = array[i];
+
+        if(item < pivot) {
+            left.push(item);
+        } else {
+            right.push(item);
+        }
+    }
+
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
